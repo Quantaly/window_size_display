f81420de5f7115c7caaffcc6b2ee859061e173d3
Kai Page <kaibug@gmail.com>
2018-09-12 10:10:47 -0500

    this is happening bro

--- script.js
+++ script.js
@@ -1,4 +1,4 @@
-setInterval(function() {
+function updoot() {
     let output = document.getElementById("output");
     output.innerHTML = "Inner: " + window.innerWidth + " x " + window.innerHeight + "<br>Outer: " + window.outerWidth + " x " + window.outerHeight;
 }, 1000);
