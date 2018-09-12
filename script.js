1d0f180bcebeecaca6eccf7968d64e08f79a3314
Kai Page <kaibug@gmail.com>
2018-09-12 13:33:03 -0500

    oh. huh.
    the worst part is, that was *actually working* before.

--- script.js
+++ script.js
@@ -1,4 +1,4 @@
 function updoot() {
     let output = document.getElementById("output");
     output.innerHTML = "Inner: " + window.innerWidth + " x " + window.innerHeight + "<br>Outer: " + window.outerWidth + " x " + window.outerHeight;
-}, 1000);
+}
